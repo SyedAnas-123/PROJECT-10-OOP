c16a5433487743a0975d7268080f28a3322908b7
SyedAnas-123 <syedmohammadanasahmed@gmail.com>
2024-07-19 00:26:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,71 @@
+import inquirer from "inquirer";
+
+//class student
+class Student {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+//class person
+class Person {
+  studens: Student[] = [];
+  addStudent(obj: Student) {
+    this.studens.push(obj);
+  }
+}
+
+//person ke array ko initiate kiya hai idhr
+const persons = new Person();
+
+const programStart = async (persons: Person) => {
+    do{
+  console.log("WELCOME !!");
+  const ans = await inquirer.prompt([
+    {
+      name: "select",
+      type: "list",
+      message: "WHO WOULD YOU LIKE TO INTERACT WITH? ",
+      choices: ["staff", "student", "exit"],
+    },
+  ]);
+
+  if (ans.select == "staff") {
+    console.log(
+      "YOU APPROACH THE STAFF ROOM . PLEASE FEEL FREE TO ASK ANY QUESTION "
+    );
+  } else if (ans.select == "student") {
+    const ans = await inquirer.prompt([
+      {
+        name: "student",
+        type: "input",
+        message: "ENTER THE STUDENTS NAME YOU WISH TO ENGAGE WITH",
+      },
+    ]);
+
+    const student = persons.studens.find((val) => val.name == ans.student);
+
+    if (!student) {
+      const name = new Student(ans.student);
+      persons.addStudent(name);
+      console.log(`HELLO I AM ${name.name} .NICE TO MEET YOU !`);
+      console.log("NEW STUDENT ADDED");
+      console.log("CURRENT STUDENT LIST :");
+      console.log(persons.studens);
+    } else {
+      console.log(`HELLO I AM ${student.name} . NICE TO SEE YOU AGAIN !`);
+      console.log("EXISTING STUDENT LIST : ");
+      console.log(persons.studens);
+    }
+  }
+
+  else if (ans.select == "exit"){
+    console.log("EXITING THE PROGRAM");
+    process.exit();
+    
+  }
+}while(true);
+};
+
+programStart(persons)
